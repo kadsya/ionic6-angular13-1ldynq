e42bdfc1faa5c197005c4bd7b3c95fd91a4edd09
kadsya <43220517+kadsya@users.noreply.github.com>
2022-09-23 09:57:43 +0000

    3 files modified, 7 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,14 @@ export class AppComponent {
   name = 'Ionic 6.2 Angular ' + VERSION.major;
 
   constructor() {}
-
+  public appPages = [
+    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   ionViewDidEnter() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouteReuseStrategy } from '@angular/router';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, IonicModule.forRoot()],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
